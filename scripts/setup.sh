ab7d82a4232384f567201932f2e09aff8ef8b45c
Hongyu Hè <hongyuhe.cs@googlemail.com>
2024-11-18 00:16:11 -0500

    Add setup script
    Signed-off-by: Hongyu Hè <hongyuhe.cs@googlemail.com>

---
+++ scripts/setup.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+set -x
+
+{
+    sudo apt update -y && sudo apt upgrade -y
+
+    unzip /data/metadata.zip -d /data/
+
+    # * Update python to 3.10
+    sudo add-apt-repository ppa:deadsnakes/ppa -y
+    sudo apt-get install python3.10 -y
+    sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1 
+    sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 2 # * Larger number, higher priority/
+    sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.10 2
+
+    # * Install pip with the right version.
+    curl -sS https://bootstrap.pypa.io/get-pip.py | python3.10
+    sudo cp /users/"$USER"/.local/bin/pip /usr/bin/
+    sudo apt install python3.10-distutils -y
+
+    python -m venv venv
+    source venv/bin/activate
+    pip install -r requirements.txt
+
+    sudo apt install htop -y
+    git config --global credential.helper store
+
+    exit 0
+}
